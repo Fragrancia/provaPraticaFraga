3d7b468e53d3f44f388e79ce52847c826fa773da
ARTHUR JOSÉ FRAGA <arthur_fraga@estudante.sesisenai.org.br>
2024-07-04 12:15:57 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header> 
+        <img src="./public/senai.png" alt="" className="logo"/>
+      <h1>CLUBES DE FUTEBOL</h1>
+      <div className='nome'>
+      <p>Nome: Arthur Jose Fraga</p>
+      <p>Turma: 3c</p>
+     </div>
+    </header>
+  );
+};
+
+export default Header;
